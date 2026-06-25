b847a847eafb445635de4dc6b1e8e0208935b752
azatdron <azatrafikov85@gmail.com>
2026-06-25 12:17:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='proficlick-calc-v7-4-57';
+const CACHE='proficlick-calc-v7-4-58';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./og-proficlick.png']).catch(()=>caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
